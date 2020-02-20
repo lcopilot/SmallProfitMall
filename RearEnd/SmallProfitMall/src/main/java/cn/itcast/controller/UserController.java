8541023dae121e20db403c5beb9f9ce80c872787
MuGe <MuGe@qq.com>
2020-02-20 11:01:54 +0800

    添加json格式数据支持

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/controller/UserController.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/controller/UserController.java
@@ -1,38 +1,42 @@
 package cn.itcast.controller;
 
 import cn.itcast.domain.User;
+import cn.itcast.response.CommonCode;
+import cn.itcast.response.QueryResponseResult;
+import cn.itcast.response.QueryResult;
 import cn.itcast.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 帐户web
  */
 @Controller
 @RequestMapping("/user")
+@ResponseBody
 public class UserController {
 
     @Autowired
     private UserService accountService;
 
     @RequestMapping("/findAll")
-    public String findAll(Model model){
+    @ResponseBody
+    public QueryResponseResult findAll(){
         System.out.println("表现层：查询所有账户...");
         // 调用service的方法
         List<User> list = accountService.findAll();
-        System.out.println(list);
-        model.addAttribute("list",list);
-        System.out.println("测试提交");
-        System.out.println("测试提交");
-        System.out.println("测试提交3");
-        return "list";
+        QueryResult<User> result = new QueryResult<>();
+        result.setList(list);
+        return new QueryResponseResult(CommonCode.SUCCESS,result);
     }
     /**
      * 保存
---
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/response/QueryResult.java
@@ -0,0 +1,22 @@
+package cn.itcast.response;
+
+import java.util.List;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @Author: mrt.
+ * @Description:
+ * @Date:Created in 2018/1/24 18:33.
+ * @Modified By:
+ */
+@Data
+@ToString
+public class QueryResult<T> {
+    //数据列表
+    private List<T> list;
+    //数据总数
+    private long total;
+
+
+}
