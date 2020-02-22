edc0012cc744e73eaf452bea922755070153c6f4
FF <fhx210114@163.com>
2020-02-22 16:57:21 +0800

    注册1

---
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/util/GetFourRandom.java
@@ -0,0 +1,24 @@
+package cn.itcast.util;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.Random;
+
+@Controller("getFourRandom")
+public class GetFourRandom {
+    /**
+     * 产生4位随机数(0000-9999)
+     * @return 4位随机数
+     */
+    public String getFourRandom(){
+        Random random = new Random();
+        String fourRandom = random.nextInt(10000) + "";
+        int randLength = fourRandom.length();
+        if(randLength<4){
+            for(int i=1; i<=4-randLength; i++)
+                fourRandom = "0" + fourRandom  ;
+        }
+        return fourRandom;
+    }
+
+}
