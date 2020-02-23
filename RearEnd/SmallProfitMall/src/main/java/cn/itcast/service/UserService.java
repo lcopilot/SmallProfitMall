2b205782050928ad2b0ad5b9c97fba984b183fab
FF <fhx210114@163.com>
2020-02-23 15:24:30 +0800

    修改密码

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/service/UserService.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/service/UserService.java
@@ -15,10 +15,18 @@ public interface UserService {
     //.根据用户手机查询用户信息
     public User findByPhone(String phone);
 
+    //.根据uid查询用户信息
+    public User findByUid(String uid);
+
+
     // 保存帐户信息
     public void saveAccount(User user);
 
+    //根据手机号码修改密码
+    public void updatePasswordPhone (String phone,String password);
 
+    //根据uid修改密码
+    public void updatePasswordUid(String uid,String password);
 
 
 }
