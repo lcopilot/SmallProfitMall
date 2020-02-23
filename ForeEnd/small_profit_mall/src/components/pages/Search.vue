e6e4ab403e7f5be02d538c44add5182f1c7e801c
MuGe <liuxianliangli@foxmail.com>
2020-02-23 12:37:13 +0800

    轮播图搜索组件布局设计完成

---
+++ ForeEnd/small_profit_mall/src/components/pages/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-main>
+    <el-row type="flex" class="row-bg" justify="center" gutter="20">
+      <el-col :span="4">
+fdsd
+      </el-col>
+      <el-col :span="6">
+dfds
+      </el-col>
+      <el-col :span="4">
+sdfs
+      </el-col>
+      <el-col :span="2">
+fds
+      </el-col>
+    </el-row>
+  </el-main>
+</template>
+
+<script>
+  export default {
+    name: "Search"
+  }
+</script>
+
+<style scoped>
+
+</style>
