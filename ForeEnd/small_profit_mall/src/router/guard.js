25937409f053d9089533fb365ff536870adf1d90
MuGe <liuxianliangli@foxmail.com>
2020-02-23 11:04:10 +0800

    增加logo

--- ForeEnd/small_profit_mall/src/router/guard.js
+++ ForeEnd/small_profit_mall/src/router/guard.js
@@ -27,8 +27,7 @@ router.beforeEach((to, from, next) => {
   if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
     if (sessionStorage.getItem("token")) {
       next();
-    }
-    else {
+    } else {
       next({
         path: '/login',
         query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
