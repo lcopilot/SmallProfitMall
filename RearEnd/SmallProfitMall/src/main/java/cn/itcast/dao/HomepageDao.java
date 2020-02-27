33f38204d2c9d93f348351eb548314419e47e694
FF <fhx210114@163.com>
2020-02-27 11:37:20 +0800

    测试

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/dao/HomepageDao.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/dao/HomepageDao.java
@@ -1,5 +1,7 @@
 package cn.itcast.dao;
 
+import cn.itcast.domain.Navigation;
+import cn.itcast.domain.Navigation_2;
 import cn.itcast.domain.RotationChart;
 
 import java.util.List;
@@ -8,4 +10,9 @@ public interface HomepageDao {
     //查询轮播图图片
     public List<RotationChart> findRotationChart();
 
+    //商品分类导航栏
+    public List<Navigation> findNavigation();
+
+    //商品分类导航栏2
+    public List<Navigation_2> findNavigation2();
 }
