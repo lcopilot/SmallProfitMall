82e249db0ba474fcbbc5d9c15bc2e1629c713364
MuGe <MuGe@qq.com>
2020-02-29 19:33:07 +0800

    测试页面完成 商品放大镜效果测试 商品图片转换测试

--- ForeEnd/small_profit_mall/src/components/pages/test.vue
+++ ForeEnd/small_profit_mall/src/components/pages/test.vue
@@ -1,7 +1,11 @@
 <template>
   <div style="width: 418px;height: 418px">
+
     <div class="pic-box"> <!--pic-box:width:500px;height:500px-->
-      <pic-zoom url="http://img.fhxasdsada.xyz/O1CN01PweeQr22AEJuIJv81_%21%211917047079.png_430x430q90.png" :scale="3"></pic-zoom>
+      <pic-zoom :url="url" :scale="3"></pic-zoom>
+    </div>
+    <div style="width: 100px;height: 100px;display: inline-block;margin-right: 4px" v-for="(img,index) in imgList" @mouseenter="enter(index)">
+        <el-image :src="img.img"></el-image>
     </div>
   </div>
 
@@ -9,14 +13,25 @@
 
 <script>
   import PicZoom from 'vue-piczoom'
+
   export default {
     name: "test",
-    components: {
-      PicZoom
-    },
+    components: {PicZoom},
     data(){
       return{
         show2:true,
+        url:'http://img.fhxasdsada.xyz/O1CN01PweeQr22AEJuIJv81_%21%211917047079.png_430x430q90.png',
+        imgList:[
+          {img:'http://img.fhxasdsada.xyz/O1CN01jRWwWw22AEIAVOrqe_%21%211917047079.jpeg_430x430q90.png'},
+          {img:'http://img.fhxasdsada.xyz/O1CN01gDQHPJ22AEDl61L9W_%21%211917047079.jpg_430x430q90.jpg'},
+          {img:'http://img.fhxasdsada.xyz/O1CN01aVSHBx22AEJy0gFkG_%21%212-item_pic.png_430x430q90.png'},
+          {img:'http://img.fhxasdsada.xyz/O1CN01PweeQr22AEJuIJv81_%21%211917047079.png_430x430q90.png'}
+        ],
+      }
+    },
+    methods:{
+      enter(index){
+        this.url=this.imgList[index].img;
       }
     }
   }
