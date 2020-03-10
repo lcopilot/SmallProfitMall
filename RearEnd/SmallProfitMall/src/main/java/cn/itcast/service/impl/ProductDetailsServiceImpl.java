a7db32963c98672d3af872e5ed20398bab4d8969
FF <fhx210114@163.com>
2020-03-10 15:33:24 +0800

    商品详细接口 新增商品版本属性

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/service/impl/ProductDetailsServiceImpl.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/service/impl/ProductDetailsServiceImpl.java
@@ -28,13 +28,11 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
     private ProductDetailsResult productDetailsResult;
 
     public List<ProductDetailsResult> findByPid(int pid) {
-        String s = String.valueOf(pid);
-        List<ProductDetailsResult>  redis = (List<ProductDetailsResult>)redisUtil.lGet(s,0,-1);
-        System.out.println(redis);
+        String ProductId = String.valueOf(pid);
+        List<ProductDetailsResult>  redis = (List<ProductDetailsResult>)redisUtil.lGet(ProductId,0,-1); //从缓存中查询
         if(redis.size()==0){
             String inventory = "";   //库存价格(转换)
             String sale = "";       //商品销量
-
             ProductBasis productDetails = productDetailsDao.findByIdBasis(pid);
             productDetailsResult=new ProductDetailsResult();
             productDetailsResult.setId(productDetails.getId());
@@ -45,21 +43,21 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
             productDetailsResult.setVideo(productDetails.getProductVideo());   //封装视频
             productDetailsResult.setFreeShipping(productDetails.getFreeShipping());//封装是否包邮
             if (productDetails.getProductInventory() > 10000) {   //库存转换
-                java.text.DecimalFormat   df=new   java.text.DecimalFormat("#.#");
+                java.text.DecimalFormat   df=new   java.text.DecimalFormat("#.#");//取小数点后一位
                 Double inventorys = productDetails.getProductInventory();
                 Double a = inventorys/10000;
                 inventory = df.format(a);
-                inventory = inventory + "W+";
+                inventory = inventory + "W+";   //库存过万转换
             }else {
-                inventory=""+productDetails.getProductInventory();
-                inventory =inventory.split("\\.")[0];
+                inventory=""+productDetails.getProductInventory(); //转为字符串类型
+                inventory =inventory.split("\\.")[0];   //取小数点前数组
             }
             if (productDetails.getProductSales()>10000){
                 java.text.DecimalFormat   df=new   java.text.DecimalFormat("#.#");
                 Double sales = productDetails.getProductSales();
                 Double a = sales/10000;
                 sale = df.format(a);
-                sale=sale+"W+";
+                sale=sale+"W+";     //销售过完转换
             }else {
                 sale=""+productDetails.getProductSales();
                 sale =sale.split("\\.")[0];
@@ -72,19 +70,14 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
             }
             productDetailsResult.setInventory(inventory);//设置库存
             productDetailsResult.setSales(sale);
-
             productDetailsResult.setImageSite(productDetailsDao.findByIdImage(pid));  //设置图片
-
             productDetailsResult.setSize(productDetailsDao.findBySize(pid));     //设置尺码
-
             productDetailsResult.setColour(productDetailsDao.findByIdColour(pid));     //设置颜色数组
-
             productDetailsResult.setCombo(productDetailsDao.findByIdCombo(pid));     //设置套装数组
-
             productDetailsResult.setSpecification(productDetailsDao.findByIdSpecification(pid));     //设置规格数组
-
             productDetailsResult.setTaste(productDetailsDao.findByIdTaste(pid));     //设置规格数组
             productDetailsResult.setKind(productDetailsDao.findByKind(pid));    //设置商品种类
+            productDetailsResult.setVersion(productDetailsDao.findByVersion(pid)); //设置商品规格
             productDetailsResult.setInventorys(new Double(productDetails.getProductInventory()).intValue());
             redisUtil.lSet(productDetailsResult.getPid(),productDetailsResult);
             System.out.println("数据库中取");
@@ -96,7 +89,5 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
         }
 
     }
-
-
 
 }
