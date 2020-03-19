3f4e124d1ac448262d5dbbca8b84b317721504c8
FF <fhx210114@163.com>
2020-03-19 11:33:53 +0800

    验证邮箱接口

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/dao/AddressDao.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/dao/AddressDao.java
@@ -1,11 +1,11 @@
-//package cn.itcast.dao;
-//
-//import cn.itcast.domain.address.Address;
-//import org.springframework.stereotype.Repository;
-////收货地址
-//@Repository
-//public interface AddressDao {
-//    //新增用户收货地址地址
-//    public int addAddress(Address address);
-//
-//}
+package cn.itcast.dao;
+
+import cn.itcast.domain.address.Address;
+import org.springframework.stereotype.Repository;
+//收货地址
+@Repository
+public interface AddressDao {
+    //新增用户收货地址地址
+    public int addAddress(Address address);
+
+}
