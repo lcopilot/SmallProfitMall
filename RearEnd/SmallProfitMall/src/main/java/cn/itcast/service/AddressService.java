41b0d7ac7f9aca65a961d559e5f31ec6005ffaf9
FF <fhx210114@163.com>
2020-03-19 17:23:33 +0800

    收件地址接口 新增收件地址新增属性

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/service/AddressService.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/service/AddressService.java
@@ -3,15 +3,17 @@ package cn.itcast.service;
 import cn.itcast.domain.address.Address;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface AddressService {
     //查询用户地址
-    public Address findById(@Param("uid")String uid);
+    public List<Address> findById(String uid);
 
     //新增用户收货地址地址
-    public int addAddress(@Param("address")Address address);
+    public int addAddress(Address address);
 
     //修改用户地址
-    public int updateAddress(@Param("address")Address address);
+    public int updateAddress(Address address);
 
     //删除用户地址
     public int deleteAddress(@Param("uid")String uid ,@Param("addressId")String addressId);
