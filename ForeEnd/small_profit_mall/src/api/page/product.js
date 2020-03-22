cb0057dd97c3b94df9e503f19a64b26f06398643
MuGe <MuGe@qq.com>
2020-03-22 19:53:42 +0800

    购物车删除,获取接口对接完成 订单页面完成, 摄像头权限获取完成,明天接入人脸识别模块 准备接入AES加密,相关方法前端测试完成

--- ForeEnd/small_profit_mall/src/api/page/product.js
+++ ForeEnd/small_profit_mall/src/api/page/product.js
@@ -4,3 +4,15 @@ import http from '../util/public'
 export const getProduct=productId=>{
   return http.requestGet('apiUrl/ProductDetails/productDetailsResult/'+productId)
 }
+//加入购物车
+export const addCart=productForm=>{
+  return http.requestPost('apiUrl/ShoppingCartController/addShoppingCart',productForm)
+}
+//获取购物车数据
+export const getShoppingCart=userId=>{
+  return http.requestGet('apiUrl/ShoppingCartController/findByUserId/'+userId)
+}
+//删除购物车
+export const removeCart=cartIdList=>{
+  return http.requestDelete('apiUrl/ShoppingCartController/deleteCart/'+cartIdList)
+}
