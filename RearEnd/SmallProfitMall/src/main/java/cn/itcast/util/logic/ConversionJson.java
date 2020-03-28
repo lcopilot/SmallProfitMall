42e659f2218da00189a8cac8421319f68f5a15a1
FF <fhx210114@163.com>
2020-03-28 14:05:38 +0800

    心跳包测试

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/util/logic/ConversionJson.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/util/logic/ConversionJson.java
@@ -22,4 +22,22 @@ public class ConversionJson {
         }
         return JSONObject.fromObject(obj).toString();
     }
+
+    /**
+     * 将json转化为实体POJO
+     * @param jsonStr
+     * @param obj
+     * @return
+     */
+    public static<T> Object JSONToObj(String jsonStr,Class<T> obj) {
+        T t = null;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            t = objectMapper.readValue(jsonStr,
+                    obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
 }
