262f63841123a6660d6178fdf5f59d3a9dc8d34f
FF <fhx210114@163.com>
2020-03-30 14:59:18 +0800

    推送消息测试

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/response/connection/QueryResultString.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/response/connection/QueryResultString.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.ToString;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * @author Kite
  * @date 2020/3/28
@@ -14,5 +16,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class QueryResultString<T> {
     private String callback ;
-    private News news;
+    private List<News> news;
 }
