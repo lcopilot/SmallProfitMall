b44a3c349fbe3ee4495fc317cbf8d2c328c701d2
FF <fhx210114@163.com>
2020-04-02 18:53:50 +0800

    修复删除最后一个地址服务器跑路问题

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/controller/AddressController.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/controller/AddressController.java
@@ -27,19 +27,24 @@ public class AddressController {
     public QueryResponseResult addAddress(@RequestBody Address address) {
         QueryResult result = new QueryResult();
         int addressId =address.getAddressId();
-        if(addressId!=0){   //判断添加还是更新 地址id不为0则是更新
+        //判断添加还是更新 地址id不为0则是更新
+        if(addressId!=0){
             int redis=addressService.updateAddress(address);
             if (redis==1) {
-                return new QueryResponseResult(CommonCode.SUCCESS, null);//更新成功
+                //更新成功
+                return new QueryResponseResult(CommonCode.SUCCESS, null);
             } else {
-                return new QueryResponseResult(CommonCode.FAIL, null);//更新失败
+                //更新失败
+                return new QueryResponseResult(CommonCode.FAIL, null);
             }
         }
         int redis=addressService.addAddress(address);
        if (redis==1) {
-            return new QueryResponseResult(CommonCode.SUCCESS, null);//添加成功
+           //添加成功
+            return new QueryResponseResult(CommonCode.SUCCESS, null);
             } else {
-            return new QueryResponseResult(CommonCode.FAIL, null);//添加失败
+           //添加失败
+            return new QueryResponseResult(CommonCode.FAIL, null);
        }
 
     }
@@ -50,7 +55,8 @@ public class AddressController {
         QueryResult result = new QueryResult();
         List<Address> address =addressService.findById(userId);
         result.setList(address);
-        return new QueryResponseResult(CommonCode.SUCCESS, result);//注册成功
+        //注册成功
+        return new QueryResponseResult(CommonCode.SUCCESS, result);
     }
 
 
@@ -59,7 +65,8 @@ public class AddressController {
     public QueryResponseResult deleteAddress(@PathVariable("userId")String userId,@PathVariable("addressId")Integer addressId,@PathVariable("defaults")Boolean defaults) {
         int redis = addressService.deleteAddress(userId, addressId, defaults);
         if (redis == 1) {
-            return new QueryResponseResult(CommonCode.SUCCESS, null);//刪除成功
+            //刪除成功
+            return new QueryResponseResult(CommonCode.SUCCESS, null);
         } else {
             return new QueryResponseResult(CommonCode.FAIL,null);
         }
@@ -71,7 +78,8 @@ public class AddressController {
     public QueryResponseResult updateDefaults(String userId,Integer addressId) {
         int redis = addressService.updateDefaults(addressId,userId,true);
         if (redis == 1) {
-            return new QueryResponseResult(CommonCode.SUCCESS, null);//修改成功
+            //修改成功
+            return new QueryResponseResult(CommonCode.SUCCESS, null);
         } else {
             return new QueryResponseResult(CommonCode.FAIL,null);
         }
