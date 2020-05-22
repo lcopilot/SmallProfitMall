740ad0c2069f05e73d0c8dcfd879b595a0a53472
FF <fhx210114@163.com>
2020-05-22 13:44:27 +0800

    商品详细页商品推荐 限制请求方式

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/controller/ProductDetailsController.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/controller/ProductDetailsController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -49,7 +50,7 @@ public class ProductDetailsController {
      * @param productId
      * @return
      */
-    @RequestMapping("/findRecommend/{productId}")
+    @RequestMapping(value = "/findRecommend/{productId}",method = RequestMethod.GET)
     public ObjectReturnResponse findRecommend(@PathVariable("productId") Integer productId){
         ObjectReturn objectReturn = new ObjectReturn();
         objectReturn.setObject( productDetailsService.findRecommend(productId));
