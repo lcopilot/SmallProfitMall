89cc1478267a67b39d49e73737a5d27b818ebb84
FF <fhx210114@163.com>
2020-05-24 11:35:07 +0800

    商品推荐反序列化

--- RearEnd/SmallProfitMall/src/main/java/cn/itcast/domain/ProductDatails/Recommend.java
+++ RearEnd/SmallProfitMall/src/main/java/cn/itcast/domain/ProductDatails/Recommend.java
@@ -2,12 +2,14 @@ package cn.itcast.domain.ProductDatails;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 商品右侧推荐
  * @author Kite
  * @date 2020/5/21
  */
-public class Recommend {
+public class Recommend implements Serializable {
     /**商品id**/
     private Integer productId;
     /**商品图片**/
