34c3de8f5b6203e85a696a70db36d47bf853b88a
FF <fhx210114@163.com>
2020-06-11 11:41:04 +0800

    查询全部商品详细信息配置信息

--- RearEnd/SmallProfitManagement/src/main/java/cn/xgtd/domain/product/ProductDistinction.java
+++ RearEnd/SmallProfitManagement/src/main/java/cn/xgtd/domain/product/ProductDistinction.java
@@ -13,25 +13,38 @@ public class ProductDistinction implements Serializable {
     private Integer productId;
     /**商品尺寸id**/
     private Integer sizeId;
+    /**尺寸内容**/
+    private String sizeContent;
     /**商品颜色id**/
     private Integer colourId;
+    /**颜色内容**/
+    private String colourContent;
     /**商品套餐id**/
     private Integer comboId;
+    /**套餐内容**/
+    private String comboContent;
     /**规格id**/
     private Integer specificationId;
+    /**规格内容**/
+    private String specificationContent;
     /**商品口味id**/
     private Integer tasteId;
+    /**口味内容**/
+    private String tasteContent;
     /**商品种类id**/
     private Integer kindId;
+    /**种类内容**/
+    private String kindContent;
     /**商品版本id**/
     private Integer versionId;
+    /**版本内容**/
+    private String versionContent;
     /**商品价格**/
     private Double productPrice;
     /**商品库存**/
     private Double ProductInventory;
     /**商品销量**/
     private Double ProductSales;
-
 
     public Integer getDistinctionId() {
         return distinctionId;
@@ -57,12 +70,28 @@ public class ProductDistinction implements Serializable {
         this.sizeId = sizeId;
     }
 
+    public String getSizeContent() {
+        return sizeContent;
+    }
+
+    public void setSizeContent(String sizeContent) {
+        this.sizeContent = sizeContent;
+    }
+
     public Integer getColourId() {
         return colourId;
     }
 
     public void setColourId(Integer colourId) {
         this.colourId = colourId;
+    }
+
+    public String getColourContent() {
+        return colourContent;
+    }
+
+    public void setColourContent(String colourContent) {
+        this.colourContent = colourContent;
     }
 
     public Integer getComboId() {
@@ -73,12 +102,28 @@ public class ProductDistinction implements Serializable {
         this.comboId = comboId;
     }
 
+    public String getComboContent() {
+        return comboContent;
+    }
+
+    public void setComboContent(String comboContent) {
+        this.comboContent = comboContent;
+    }
+
     public Integer getSpecificationId() {
         return specificationId;
     }
 
     public void setSpecificationId(Integer specificationId) {
         this.specificationId = specificationId;
+    }
+
+    public String getSpecificationContent() {
+        return specificationContent;
+    }
+
+    public void setSpecificationContent(String specificationContent) {
+        this.specificationContent = specificationContent;
     }
 
     public Integer getTasteId() {
@@ -89,6 +134,14 @@ public class ProductDistinction implements Serializable {
         this.tasteId = tasteId;
     }
 
+    public String getTasteContent() {
+        return tasteContent;
+    }
+
+    public void setTasteContent(String tasteContent) {
+        this.tasteContent = tasteContent;
+    }
+
     public Integer getKindId() {
         return kindId;
     }
@@ -97,12 +150,28 @@ public class ProductDistinction implements Serializable {
         this.kindId = kindId;
     }
 
+    public String getKindContent() {
+        return kindContent;
+    }
+
+    public void setKindContent(String kindContent) {
+        this.kindContent = kindContent;
+    }
+
     public Integer getVersionId() {
         return versionId;
     }
 
     public void setVersionId(Integer versionId) {
         this.versionId = versionId;
+    }
+
+    public String getVersionContent() {
+        return versionContent;
+    }
+
+    public void setVersionContent(String versionContent) {
+        this.versionContent = versionContent;
     }
 
     public Double getProductPrice() {
