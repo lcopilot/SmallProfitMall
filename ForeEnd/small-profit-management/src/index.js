2e0bc88218a5af25036b95430d9e38cbfcf361cf
MuGe <50407640+liumuge@users.noreply.github.com>
2020-06-16 16:30:33 +0800

    修复redux页面刷新数据会被清空

--- ForeEnd/small-profit-management/src/index.js
+++ ForeEnd/small-profit-management/src/index.js
@@ -4,17 +4,20 @@ import App from './App'
 import storageUtils from "./utils/storageUtils";
 import {Provider} from 'react-redux'
 import store from "./store";
+import {persistor} from './store'
 import * as ActionCreators from "./store/actionCreators";
 import zhCN from 'antd/es/locale/zh_CN';
 import {ConfigProvider} from "antd";
-
+import {PersistGate} from 'redux-persist/lib/integration/react';
 //读取 local 的数据
 store.dispatch(ActionCreators.setUser(storageUtils.getUser()));
 
 const AppReactRedux = (
     <ConfigProvider locale={zhCN}>
       <Provider store={store}>
-        <App/>
+        <PersistGate loading={null} persistor={persistor}>
+          <App/>
+        </PersistGate>
       </Provider>
     </ConfigProvider>
 );
